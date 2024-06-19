f55e6af93c2836471b6b8a3bc7de575b5c985c02
muk8620 <muk8620@gmail.com>
2024-06-19 16:47:30 +0900

    mybatis설치 및 세팅

--- src/main/java/com/example/demo/dao/ArticleDao.java
+++ src/main/java/com/example/demo/dao/ArticleDao.java
@@ -1,55 +1,23 @@
 package com.example.demo.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.vo.Article;
 
-@Component
-public class ArticleDao {
+@Mapper
+public interface ArticleDao {
 	
-	private int lastArticleId;
-	private List<Article> articles;
-	
-	public ArticleDao() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
-		makeTestData();
-	}
-	
-	public Article getArticleById(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-	
-	public void makeTestData() {
-		for (int i = 1; i <= 5; i++) {
-			doWrite("제목" + i, "내용" + i);
-		}
-	}
+	public void makeTestData();
 
-	public Article doWrite(String title, String body) {
-		Article article = new Article(++lastArticleId, title, body);
-		articles.add(article);
-		return article;
-	}
+	public Article doWrite(String title, String body);
 	
-	public void doDelete(Article foundArticle) {
-		articles.remove(foundArticle);
-	}
+	public void doDelete(Article foundArticle);
+	
+	public Article getArticleById(int id);
 		
-	public void doModify(Article foundArticle, String title, String body) {
-		foundArticle.setTitle(title);
-		foundArticle.setBody(body);
-	}
+	public void doModify(Article foundArticle, String title, String body);
 
-	public List<Article> showList() {
-		return articles;
-	}
+	public List<Article> showList();
 }
