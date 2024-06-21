f39848f6bc71cbc57773e7678fba5adea2a0f2cf
muk8620 <muk8620@gmail.com>
2024-06-21 16:00:23 +0900

    session활용하여 doLogin, doLogout

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -9,6 +9,9 @@ import com.example.demo.util.Util;
 import com.example.demo.vo.Member;
 import com.example.demo.vo.ResultData;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class UsrMemberController {
 	
@@ -52,5 +55,49 @@ public class UsrMemberController {
 		int id = memberService.getLastInsertId();
 		
 		return ResultData.from("S-1", String.format("%s님이 가입되었습니다", nickname), memberService.getMemberById(id));
+	}
+	
+	
+	@GetMapping("/usr/member/doLogin")
+	@ResponseBody
+	public ResultData<String> doLogin(HttpSession session, String loginId, String loginPw) {
+		
+		if (session.getAttribute("loginedMemberId") != null) {
+			return ResultData.from("F-L", "로그아웃 후 이용해주세요.");
+		}
+		
+		if (Util.isEmpty(loginId)) {
+			return ResultData.from("F-1", "아이디를 입력해주세요");
+		}
+		
+		if (Util.isEmpty(loginPw)) {
+			return ResultData.from("F-2", "비밀번호를 입력해주세요");
+		}
+		
+		Member member = memberService.getMemberByLoginId(loginId);
+		
+		if (member == null) {
+			return ResultData.from("F-3", String.format("%s은(는) 존재하지 않는 아이디입니다", loginId));
+		}
+		
+		if (!member.getLoginPw().equals(loginPw)) {
+			return ResultData.from("F-4", "비밀번호를 확인해주세요"); 
+		}
+		
+		session.setAttribute("loginedMemberId", loginId);
+		return ResultData.from("S-1", String.format("%s님 환영합니다", member.getNickname()), (String) session.getAttribute("loginedMemberId"));
+	}
+	
+	@GetMapping("/usr/member/doLogout")
+	@ResponseBody
+	public ResultData doLogout(HttpSession session) {
+		
+		if (session.getAttribute("loginedMemberId") == null) {
+			return ResultData.from("F-L", "로그인 후 이용해주세요.");
+		}
+		
+		session.removeAttribute("loginedMemberId");
+		
+		return ResultData.from("S-1", "정상적으로 로그아웃 되었습니다.");
 	}
 }
